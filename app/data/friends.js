ed694abac5817d040c14cd5c0e64900eed6c70fc
kwhitebloom <29611438+calemonte@users.noreply.github.com>
2019-02-15 20:48:19 -0500

    Clean up overly nested new Friends objects.

--- app/data/friends.js
+++ app/data/friends.js
@@ -9,17 +9,15 @@ class Friend {
     }
 }
 
-let allFriends = {
+let allFriends = [
+    new Friend("Mark", "https://m.media-amazon.com/images/M/MV5BNDQyNzMzZTMtYjlkNS00YzFhLWFhMTctY2M4YmQ1NmRhODBkXkEyXkFqcGdeQXVyNjcyNzgyOTE@._V1_UY317_CR20,0,214,317_AL_.jpg", "Actor with passion for starring as a big green dude in action flicks.", [5, 1, 10, 9, 5]), 
 
-    friend1: new Friend("Mark", "https://m.media-amazon.com/images/M/MV5BNDQyNzMzZTMtYjlkNS00YzFhLWFhMTctY2M4YmQ1NmRhODBkXkEyXkFqcGdeQXVyNjcyNzgyOTE@._V1_UY317_CR20,0,214,317_AL_.jpg", "Actor with passion for starring as a big green dude in action flicks.", [5, 1, 10, 9, 5]),
-    
-    friend2: new Friend("Mary", "https://upload.wikimedia.org/wikipedia/commons/thumb/c/c4/Mary_Poppins5.jpg/250px-Mary_Poppins5.jpg", "Child care expert who loves to watch the sun set over a dusty road", [5, 8, 1, 9, 10]), 
-    
-    friend3: new Friend("William", "https://upload.wikimedia.org/wikipedia/commons/thumb/0/0a/Prince_William_and_Duchess_Kate_of_Cambridge_visits_Sweden_02_%28cropped_2%29.jpg/220px-Prince_William_and_Duchess_Kate_of_Cambridge_visits_Sweden_02_%28cropped_2%29.jpg", "Posh dad with a pretty famous pedigree", [10, 10, 10, 10, 10]),
-    
-    friend4: new Friend("Serena", "https://upload.wikimedia.org/wikipedia/commons/thumb/4/4b/Serena_Williams_at_2013_US_Open.jpg/440px-Serena_Williams_at_2013_US_Open.jpg", "Superstar athlete and mom with interest in racquet sports", [4, 10, 1, 10, 5]),
+    new Friend("Mary", "https://upload.wikimedia.org/wikipedia/commons/thumb/c/c4/Mary_Poppins5.jpg/250px-Mary_Poppins5.jpg", "Child care expert who loves to watch the sun set over a dusty road", [5, 8, 1, 9, 10]),
 
-}
+    new Friend("William", "https://upload.wikimedia.org/wikipedia/commons/thumb/0/0a/Prince_William_and_Duchess_Kate_of_Cambridge_visits_Sweden_02_%28cropped_2%29.jpg/220px-Prince_William_and_Duchess_Kate_of_Cambridge_visits_Sweden_02_%28cropped_2%29.jpg", "Posh dad with a pretty famous pedigree", [10, 10, 10, 10, 10]),
+
+    new Friend("Serena", "https://upload.wikimedia.org/wikipedia/commons/thumb/4/4b/Serena_Williams_at_2013_US_Open.jpg/440px-Serena_Williams_at_2013_US_Open.jpg", "Superstar athlete and mom with interest in racquet sports", [4, 10, 1, 10, 5])
+];
 
 module.exports.Friend = Friend;
 module.exports.allFriends = allFriends;
