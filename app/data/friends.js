44bb1bbadeee767678ba333321b6047744ab442c
kwhitebloom <29611438+calemonte@users.noreply.github.com>
2019-02-15 20:12:55 -0500

    Add strict mode, add basic Friends class

---
+++ app/data/friends.js
@@ -0,0 +1,10 @@
+"use strict";
+
+module.exports = class Friend {
+    constructor() {
+        this.name = name;
+        this.photo = photo;
+        this.bio = bio;
+        this.scores = scores;
+    }
+}
--- app/routing/apiRoutes.js
+++ app/routing/apiRoutes.js
@@ -1,3 +1,5 @@
+"use strict";
+
 const path = require("path");
 const friends = require("../data/friends");
 
