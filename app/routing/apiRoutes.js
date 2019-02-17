c580c11e051aeabf660a57a2df35a67f027cc7d1
kwhitebloom <29611438+calemonte@users.noreply.github.com>
2019-02-17 12:57:27 -0500

    Add basic compability function for selecting and returning best friend match.

--- app/routing/apiRoutes.js
+++ app/routing/apiRoutes.js
@@ -13,17 +13,34 @@ module.exports = function(app) {
     .post((req, res) => {
         const data = req.body;
         const newFriend = new friends.Friend(data.name, data.photo, data.bio, data.scores);
+        const match = calculateCompability(newFriend, friends.allFriends);
+
         friends.allFriends.push(newFriend);
         console.log("API post request successful.");
-        res.json(newFriend);
+
+        res.json(match);
     });
 
+    // Calculate the combability of the available friends and return the best match.
     function calculateCompability(newFriend, allFriends) {
-        let newFriendScore = 0;
-        for (let scores in newFriend) {
+        let matchTotal = 100;
+        let finalMatch = {};
+
+        for (let i = 0; i < allFriends.length; i++) {
+            
+            // Return false if we are trying to match the same friend.
+            if (allFriends[i].id === newFriend.id) {
+                return false;
+            }
+
+            const currentDifference = Math.abs(allFriends[i].totalScore - newFriend.totalScore);
+
+            if (currentDifference < matchTotal) {
+                matchTotal = currentDifference;
+                finalMatch = allFriends[i];
+            }
 
         }
-        
+        return finalMatch;
     }
-    
 };
